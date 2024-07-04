5968966d07690ebb063321c11ce5aedcbd1cd801
Deep <gagandeepsandhu@protonmail.com>
2024-07-04 01:07:48 -0400

    nvim fixes

--- nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/lua/josean/plugins/colorscheme.lua
@@ -1,8 +1,30 @@
+--return {
+-- "nyoom-engineering/oxocarbon.nvim",
+--config = function()
+-- vim.opt.background = "dark" -- set this to dark or light
+-- vim.cmd("colorscheme oxocarbon")
+--vim.opt.fillchars = { eob = " " }
+--end,
+--}
+
+--return {
+-- "folke/tokyonight.nvim",
+--lazy = false,
+--priority = 1000,
+--  opts = {},
+-- config = function()
+--  vim.cmd("colorscheme tokyonight-storm")
+-- vim.opt.fillchars = { eob = " " }
+-- end,
+-- }
+
 return {
-  "nyoom-engineering/oxocarbon.nvim",
+  "shaunsingh/nord.nvim", --'"sainnhe/everforest",
+  lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
-    vim.opt.background = "dark" -- set this to dark or light
-    vim.cmd("colorscheme oxocarbon")
+    vim.cmd("colorscheme nord")
     vim.opt.fillchars = { eob = " " }
   end,
 }
